2ddf3271a4dffcdcc8cf7d5e52d351b02beeb5d0
최승은 <liberalfilm@gmail.com>
2022-05-09 22:24:16 +0900

    2022-05-09-Mon 22:23

---
+++ src/com/javaex/function/Concept.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<script type="text/javascript">
+		function get_member1(){
+            return 'egoing';
+	    }
+		
+        funtion get_member2(){
+            return 'k8805'; //return키위드 등장: 1>위 함수를 종료시킴, 2>뒤에 있고 ;앞에 있는 값을 위 함수의 출력값으로 반환함(함수 바깥 영역으로 뱉어냄) 
+        }
+
+        alert(get_member1());
+        alert(get_member2());
+
+
+	</script>
+</body>
+</html>
